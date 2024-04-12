fd1ada1cc1948cca77eeda360c85dd8d0a8869e2
Dhananjay Kumar <64013411+dhananjay229@users.noreply.github.com>
2024-04-12 17:07:40 +0530

    Update Header.jsx
    fixed header mobile view responsive

--- src/components/utility/Header.jsx
+++ src/components/utility/Header.jsx
@@ -41,19 +41,24 @@ export default function Header() {
   }
 
   return (
-    <div className=" body-width dark:bg-zinc-900 w-full flex flex-col items-center lg:mx-[50px]" >
-      <div className="w-1/2 pb-2 mt-2 flex flex-row items-center justify-center ml-24 self-start md:self-center">
-        <div className="border shadow-sm my-4 rounded-full  md:flex flex-row justify-center items-center text-slate-800 dark:bg-zinc-800  dark:border-zinc-600">
-          {menuLinks.map(i => (
+    <div className=" body-width dark:bg-zinc-900 w-full flex flex-col  items-center lg:mx-[50px]">
+      <div className="mt-2 flex flex-row items-center justify-center self-start md:self-center">
+        <div className="border shadow-sm my-4 lg:mx-4 rounded-full md:flex flex-row justify-evenly items-center text-slate-800 dark:bg-zinc-800  dark:border-zinc-600">
+          {menuLinks.map((i) => (
             <Link
-              className=" px-2 z-40 py-2 text-lg font-medium text-zinc-800 dark:text-white dark:hover:text-teal-400  bg-gradient dark:border-0 "
-              href={i.link} key={i.title}>
+              className=" px-2 z-40 py-2 lg:text-lg lg:font-medium text-zinc-800 dark:text-white dark:hover:text-teal-400 bg-gradient dark:border-0 "
+              href={i.link}
+              key={i.title}
+            >
               {i.title}
             </Link>
           ))}
         </div>
-        <div className=" flex flex-row ">
-          <button className=" hover:bg-slate-100 border shadow-sm rounded-full lg:text-xl h-10 lg:px-4 px-2 mx-2 dark:hover:bg-zinc-800 dark:text-teal-400 dark:ring-white dark:hover:ring-white dark:border-zinc-600" onClick={() => handleToggle()}>
+        <div className=" flex flex-row ml-2">
+          <button
+            className=" hover:bg-slate-100 border shadow-sm rounded-full lg:text-xl h-10 lg:px-4 lg:mx-2 px-2 dark:hover:bg-zinc-800 dark:text-white dark:ring-white dark:hover:ring-white dark:border-zinc-600"
+            onClick={() => handleToggle()}
+          >
             <div className="hidden dark:flex">
               <BsMoonStars />
             </div>

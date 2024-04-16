54f5694fdbd3d0da6d6a1283bbc3db39bd9c9548
Dhananjay Kumar <64013411+dhananjay229@users.noreply.github.com>
2024-04-16 19:09:17 +0530

    Update Header.jsx
    Fixed Navbar mobile view

--- src/components/utility/Header.jsx
+++ src/components/utility/Header.jsx
@@ -1,75 +1,110 @@
 "use client";
-import Link from "next/link"
+import Link from "next/link";
 import { useState } from "react";
-import { BsMoonStars, BsSun } from 'react-icons/bs'
-
+import { BsMoonStars, BsSun } from "react-icons/bs";
 
 export const menuLinks = [
   {
     title: "About",
-    link: "/about"
+    link: "/about",
   },
   {
     title: "Articles",
-    link: "/articles"
+    link: "/articles",
   },
   {
     title: "Projects",
-    link: "/projects"
+    link: "/projects",
   },
   {
     title: "Speaking",
-    link: "/speaking"
+    link: "/speaking",
   },
   {
     title: "Uses",
-    link: "/uses"
-  }
-]
+    link: "/uses",
+  },
+];
 
 export default function Header() {
-
   const handleToggle = () => {
     if (!localStorage.theme) {
-      document.documentElement.classList.add('dark')
-      localStorage.setItem("theme", "dark")
+      document.documentElement.classList.add("dark");
+      localStorage.setItem("theme", "dark");
     } else {
-      document.documentElement.classList.remove('dark')
-      localStorage.removeItem("theme")
+      document.documentElement.classList.remove("dark");
+      localStorage.removeItem("theme");
     }
+  };
 
-  }
+  const [navbar, setNavbar] = useState(false);
 
   return (
-    <div className=" body-width dark:bg-zinc-900 w-full flex flex-col  items-center lg:mx-[50px]">
-      <div className="mt-2 flex flex-row items-center justify-center self-start md:self-center">
-        <div className="border shadow-sm my-4 lg:mx-4 rounded-full md:flex flex-row justify-evenly items-center text-slate-800 dark:bg-zinc-800  dark:border-zinc-600">
-          {menuLinks.map((i) => (
-            <Link
-              className=" px-2 z-40 py-2 lg:text-lg lg:font-medium text-zinc-800 dark:text-white dark:hover:text-teal-400 bg-gradient dark:border-0 "
-              href={i.link}
-              key={i.title}
+    
+    <nav className=" dark:bg-zinc-900 text-slate-800 body-width z-10 w-full">
+      <div className="justify-center lg:px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8 ">
+        <div className="flex items-center justify-between py-2 md:py-2 md:block">
+          <div className="md:hidden">
+            <button
+              className="p-2 dark:text-white text-black rounded-md border-2 ml-52 focus:border-gray-400 focus:border"
+              onClick={() => setNavbar(!navbar)}
             >
-              {i.title}
-            </Link>
-          ))}
+              {navbar ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6 text-white"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              ) : (
+                <h1>Menu</h1>
+              )}
+            </button>
+            <button
+              className=" hover:bg-slate-100 border-2 rounded-full lg:text-xl h-10 px-4 mx-4 dark:hover:bg-zinc-800 dark:text-teal-400 dark:ring-white dark:hover:ring-white dark:border-zinc-600"
+              onClick={() => handleToggle()}
+            >
+              <div className="hidden dark:flex">
+                <BsMoonStars />
+              </div>
+              <div className="dark:hidden flex">
+                <BsSun />
+              </div>
+            </button>
+          </div>
         </div>
-        <div className=" flex flex-row ml-2">
-          <button
-            className=" hover:bg-slate-100 border shadow-sm rounded-full lg:text-xl h-10 lg:px-4 lg:mx-2 px-2 dark:hover:bg-zinc-800 dark:text-teal-400 dark:ring-white dark:hover:ring-white dark:border-zinc-600"
-            onClick={() => handleToggle()}
-          >
-            <div className="hidden dark:flex">
-              <BsMoonStars />
-            </div>
-            <div className="dark:hidden flex">
-              <BsSun />
-            </div>
-          </button>
+        <div
+          className={`lg:border md:border shadow-sm lg:my-4 p-2 lg:mx-4 lg:rounded-full md:rounded-full md:flex flex-row justify-evenly items-center text-slate-800 lg:dark:bg-zinc-800  dark:border-zinc-600 ${
+            navbar ? "block" : "hidden"
+          }`}
+        >
+          <ul className="items-center justify-center space-y-4 md:flex md:space-x-6 md:space-y-0 mb-lg-0">
+            {menuLinks.map((item) => (
+              <li className="cursor-pointer font-header dark:text-white text-black dark:hover:text-teal-400 md:text-xl">
+                <a href={item.link}>
+                  <h1>{item.title}</h1>
+                </a>
+              </li>
+            ))}
+          </ul>          
         </div>
+        <button
+          className=" hidden lg:block hover:bg-slate-100 border shadow-sm rounded-full lg:text-xl h-10 lg:px-4 lg:mx-2 px-2 dark:hover:bg-zinc-800 dark:text-teal-400 dark:ring-white dark:hover:ring-white dark:border-zinc-600 "
+          onClick={() => handleToggle()} >
+          <div className="hidden dark:flex">
+            <BsMoonStars />
+          </div>
+          <div className="dark:hidden flex">
+            <BsSun />
+          </div>
+        </button>
       </div>
-    </div>
-
-
-  )
+    </nav>
+  );
 }
